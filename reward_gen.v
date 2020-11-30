8b4d5f28c61454fc3bf96d510c9c3b7b08e0e871
Arief Hirmanto <ariefhirmanto7@gmail.com>
2020-11-30 21:26:46 +0700

    add comment

--- reward_gen.v
+++ reward_gen.v
@@ -4,6 +4,7 @@ module reward_gen(current_state, reward);
 
     always @(*)
     begin
+        // learning agent win
        if((current_state[1:0] == 2'd1 && current_state[9:8] == 2'd1 && current_state[17:16] == 2'd1) || 
         (current_state[5:4] == 2'd1 && current_state[9:8] == 2'd1 && current_state[13:12] == 2'd1) || 
             (current_state[1:0] == 2'd1 && current_state[3:2] == 2'd1 && current_state[5:4] == 2'd1) || 
@@ -15,6 +16,7 @@ module reward_gen(current_state, reward);
             begin
                 reward <= 2;
             end
+        // learning agent lose
         else if((current_state[1:0] == 2'd2 && current_state[9:8] == 2'd2 && current_state[17:16] == 2'd2) || 
             (current_state[5:4] == 2'd2 && current_state[9:8] == 2'd2 && current_state[13:12] == 2'd2) || 
                 (current_state[1:0] == 2'd2 && current_state[3:2] == 2'd2 && current_state[5:4] == 2'd2) || 
@@ -26,12 +28,14 @@ module reward_gen(current_state, reward);
             begin
                 reward <= -2;
             end
+        // match draw
         else if (!((current_state[1:0] == 2'd0) || (current_state[3:2] == 2'd0) || (current_state[5:4] == 2'd0) ||
                     (current_state[7:6] == 2'd0) || (current_state[9:8] == 2'd0) || (current_state[11:10] == 2'd0) ||
                     (current_state[13:12] == 2'd0) || (current_state[15:14] == 2'd0) || (current_state[17:16] == 2'd0))) 
             begin
                 reward <= 0;
             end
+        // continue
         else
             begin
                 reward <= 1;
