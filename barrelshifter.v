35a42cb103ed5c6defac0a2349bafd54ab280ab4
Arief Hirmanto <ariefhirmanto7@gmail.com>
2020-12-25 15:59:59 +0700

    add barrel shifter

--- barrelshifter.v
+++ barrelshifter.v
@@ -4,5 +4,5 @@ module barrelshifter(data_in, bit_1, bit_2, shifted);
     input [3:0] bit_2;
     output [15:0] shifted;
 
-    assign shifted = (data_in >> bit_1) + (data_in >> bit_2);
+    assign shifted = ((bit_1 != 4'd0) ? (data_in >> bit_1) : 15'd0) + ((bit_2 != 4'd0) ? (data_in >> bit_2) : 15'd0);
 endmodule
--- q_updater.v
+++ q_updater.v
@@ -6,10 +6,9 @@ module Q_updater(Q, max_Q, gamma, alfa, reward, Q_new);
     input [15:0] Q, max_Q, gamma, alfa, reward;
     reg [15:0] eq_1; //eq_1 = gamma * maxQ
     reg [15:0] eq_2; //
-    reg [15:0] temp = eq_1 + reward - Q; 
+    reg [15:0] temp; 
     output [15:0] Q_new;
-
-    output [15:0] Q_new;
+    temp <= eq_1 + reward - Q
 
     barrelshifter bs_1(
         .data_in(max_Q), 
